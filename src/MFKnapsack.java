8109e7680d9835e0e070f6bbb4b48cdf858296a7
Giampiero <glvanzzini@gmail.com>
2016-12-01 09:35:48 -0600

    Almost done

--- src/MFKnapsack.java
+++ src/MFKnapsack.java
@@ -2,18 +2,23 @@
  * Created by Giampiero on 11/29/2016.
  */
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.io.File;
 public class MFKnapsack {
     private static int[] Weights;
     private static int[] Values;
     private static int[][] F;
-    private static int[] itemsToBring = new int[4]; //this will be a list of which items are to be brought
-    private static final int W = 5;
+
+    //array of items to bring index inside of the knapsack code
+    private static int W;
     public static void main (String[] args) throws FileNotFoundException {
         Scanner in = new Scanner(new File(args[0]));
-        //in.nextLine();
+       // in.nextLine();
+        in.nextInt();
         int n = 0;
+
         while(in.hasNextInt()){
             in.nextInt();
             n++;
@@ -21,13 +26,14 @@ public class MFKnapsack {
         n = n/2;
         in.close();
         in = new Scanner(new File(args[0]));
+        W = in.nextInt();
         Weights = new int[n+1];
         Values = new int[n+1];
         F = new int[n+1][W+1];
         for(int i = 1; i < n+1; i++){
             Weights[i] = in.nextInt();
             Values[i] = in.nextInt();
-            System.out.println("Weight: " + Weights[i] + "\tValues: " + Values[i]);
+            //System.out.println("Weight: " + Weights[i] + "\tValues: " + Values[i]);
         }
         in.close();
         for(int i = 0; i < n+1; i++){
@@ -39,14 +45,19 @@ public class MFKnapsack {
                 }
             }
         }
+        long startTime = System.nanoTime();
         int value = knapsack(n, W);
-        System.out.println(value);
+        long endTime = System.nanoTime() - startTime;
+        System.out.println("Solved knapsack table:");
         for(int i = 0; i < n+1; i++) {
             for (int j = 0; j < W +1; j++) {
-                System.out.print(F[i][j] + " ");
+                System.out.print(F[i][j] + "\t");
             }
             System.out.println();
         }
+        PrintOptimalSolution(F, Weights, n, W);
+        System.out.println("The maximum cost that can be carried is: " + value + "; completed in " + endTime + " nanoseconds");
+
     }
 
     private static int knapsack(int i, int j) {
@@ -57,9 +68,12 @@ public class MFKnapsack {
                value = knapsack(i - 1, j);
            } else{
                value = max(knapsack(i - 1, j), (Values[i] + knapsack(i-1, j - Weights[i])));
+
            }
+
            F[i][j] = value;
        }
+
         return F[i][j];
     }
 
@@ -69,6 +83,32 @@ public class MFKnapsack {
         }else{
             return j;
         }
+    }
+
+    public static void PrintOptimalSolution(int[][] MF, int[] w, int n, int c) {
+        //MF is the matrix/table, w[] is the array with the weights, n is the num of items and c is the capacity
+        ArrayList<Integer> opt = new ArrayList<Integer>();
+        int j = c;
+        int i = n;
+        while (i > 0) {
+            //System.out.println("J: " + j);
+            if (j == 0)
+                break;
+            else if (MF[i][j] != MF[i-1][j]) {
+                //System.out.println("I: " + i);
+                opt.add(i);
+                j = j-w[i];
+            }
+
+            i = i-1;
+
+        }
+        System.out.print("Items: ");
+        for (int p = 0; p < opt.size();p++) {
+            System.out.print(opt.get(p) + " ");
+        }
+        System.out.println();
+
     }
 
 
