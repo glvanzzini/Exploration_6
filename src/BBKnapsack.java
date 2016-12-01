2ec8fd6236d0d2ac5d4d1ed822e6bad6e356cf26
jacobmurillo <jacobmurillo.jm@gmail.com>
2016-12-01 11:51:20 -0600

    Finally BBK
    Best Burger King

--- src/BBKnapsack.java
+++ src/BBKnapsack.java
@@ -1,110 +1,141 @@
-/**
- * Created by jacobmurillo on 11/30/16.
- *
- * Since we are using a state-space tree for this algorithm,
- * we would be using our own data structure to solve the
- * Branch-and-Bound Knapsack.
- *
- */
 
 import java.util.*;
 import java.io.*;
 
 public class BBKnapsack{
-    static int[] Weights;
-    static int[] Values;
-    static int[][] F;
 
-    private static final int W = 5;
+    public static List<Item> items = new LinkedList<>();
+    private static int W;
+
     public static void main(String args[]) throws FileNotFoundException{
+
         Scanner in = new Scanner(new File(args[0]));
-        //in.nextLine();
-        int n = 0;
-        while(in.hasNextInt()){
-            in.nextInt();
-            n++;
-        }
-        n = n/2;
-        in.close();
-        in = new Scanner(new File(args[0]));
-        Weights = new int[n+1];
-        Values = new int[n+1];
-        F = new int[n+1][W+1];
-        for(int i = 1; i < n+1; i++){
-            Weights[i] = in.nextInt();
-            Values[i] = in.nextInt();
-            System.out.println("Weight: " + Weights[i] + "\tValues: " + Values[i]);
-        }
-        in.close();
-        for(int i = 0; i < n+1; i++){
-            for(int j = 0; j < W+1; j++){
-                if(i == 0 || j == 0){
-                    F[i][j] = 0;
-                } else{
-                    F[i][j] = -1;
-                }
-            }
-        }
-        int value = knapsack(n, W);
-        System.out.println(value);
-        for(int i = 0; i < n+1; i++) {
-            for (int j = 0; j < W +1; j++) {
-                System.out.print(F[i][j] + " ");
-            }
-            System.out.println();
+        W = in.nextInt();
+        while(in.hasNext()) {
+            Item item = new Item();
+            item.weight = in.nextDouble();
+            item.value = in.nextDouble();
+            items.add(item);
         }
 
-    }
+        long startTime = System.nanoTime();
 
-    private static int knapsack(int i, int j){
-        return 0;
+        branchAndBoundKnapsack knapsack = new branchAndBoundKnapsack(items, W);
+        System.out.print(knapsack.solve());
+        long endTime = System.nanoTime() - startTime;
+
+        System.out.println("; completed in " + endTime + " nanoseconds");
     }
 
     // class dedicated for the BBKnapsack
-    public class BranchAndBoundKnapsack{
+    public static class branchAndBoundKnapsack {
 
-        private class Node implements Comparable<Node>{
+        public List<Item> items;
+        public int capacity;
+
+        public branchAndBoundKnapsack(List<Item> items, int capacity) {
+            this.items = items;
+            this.capacity = capacity;
+        }
+
+        private class Node implements Comparable<Node> {
             public int h;
             List<Item> taken;
-            public double bound, value, weight;
+            public double bound;
+            public double value;
+            public double weight;
 
-            public Node(){
-                taken = new ArrayList<Item>();
+            public Node() {
+                taken = new ArrayList<>();
             }
 
-            public int compareTo(Node secondNode){
-                return (int)(secondNode.bound - this.bound);
+            public Node(Node parent) {
+                h = parent.h + 1;
+                taken = new ArrayList<>(parent.taken);
+                bound = parent.bound;
+                value = parent.value;
+                weight = parent.weight;
             }
 
+            public int compareTo(Node other) {
+                return (int) (other.bound - bound);
+            }
+
+            public void computeBound() {
+                int i = h;
+                double w = weight;
+                bound = value;
+                Item item;
+                do {
+                    item = items.get(i);
+                    if (w + item.weight > capacity) break;
+                    w += item.weight;
+                    bound += item.value;
+                    i++;
+                } while (i < items.size());
+                bound += (capacity - w) * (item.value / item.weight);
+            }
+        }
+
+        public String solve() {
+            Collections.sort(items, Item.byRatio());
+            Node best = new Node();
+            Node root = new Node();
+            root.computeBound();
+            PriorityQueue<Node> q = new PriorityQueue<Node>();
+            q.offer(root);
+
+            while (!q.isEmpty()) {
+                Node node = q.poll();
+
+                if (node.bound > best.value && node.h < items.size() - 1) {
+
+                    Node with = new Node(node);
+                    Item item = items.get(node.h);
+                    with.weight += item.weight;
+
+                    if (with.weight <= capacity) {
+
+                        with.taken.add(items.get(node.h));
+                        with.value += item.value;
+                        with.computeBound();
+
+                        if (with.value > best.value) {
+                            best = with;
+                        }
+                        if (with.bound > best.value) {
+                            q.offer(with);
+                        }
+                    }
+
+                    Node without = new Node(node);
+                    without.computeBound();
+
+                    if (without.bound > best.value) {
+                        q.offer(without);
+                    }
+                }
+            }
+
+            return "The maximum cost that can be carried is: " + best.value;
         }
     }
 
-    public class Item{
-
+    public static class Item {
         public int label;
         public double value;
         public double weight;
 
-        public double getRatio(){
+        public static Comparator<Item> byLabel() {
+            return (i1, i2) -> i1.label - i2.label;
+        }
+
+        public static Comparator<Item> byRatio() {
+            return (i1, i2) -> Double.compare(i2.getRatio(), i1.getRatio());
+        }
+
+        public double getRatio() {
             return value / weight;
-        }
-
-        public Comparator<Item> byLabel(){
-            return new Comparator<Item>() {
-                @Override
-                public int compare(Item o1, Item o2) {
-                    return o1.label - o2.label;
-                }
-            };
-        }
-
-        public Comparator<Item> byRatio(){
-            return new Comparator<Item>() {
-                @Override
-                public int compare(Item o1, Item o2) {
-                    return Double.compare(o2.getRatio(), o1.getRatio());
-                }
-            };
         }
     }
 
